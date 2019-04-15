bb4fe2edab7221612709d8c5ad10df9f761924fa
lsi <lsi@lsibdoc>
2019-04-15 09:00:40 +0200

    Eskere modu deabru

--- 1.HEARTHSTONE/src/kartak/Sorginkeria.java
+++ 1.HEARTHSTONE/src/kartak/Sorginkeria.java
@@ -15,11 +15,11 @@ public abstract class Sorginkeria extends Karta
 	public abstract void egikarituSorginkeria();
 	public boolean edonoriErasoDiezaioke (ListaKartak pLista)
 	{
-		boolean emaitza = false;
+		boolean emaitza = false; 
 		return emaitza;
 	}
 	public void inprimatu()
 	{
-		//TODO
+		//TODO tutu
 	}
 }
